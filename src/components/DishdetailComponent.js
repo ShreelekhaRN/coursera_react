c8e1664baace8a98e64ea45584aa4cba9471fb88
ShreelekhaRN <shreelekharn8@gmail.com>
2020-06-25 18:53:59 +0530

    single page application part 2

--- src/components/DishdetailComponent.js
+++ src/components/DishdetailComponent.js
@@ -1,7 +1,8 @@
 import React, { Component } from 'react';
-import { Card, CardImg, CardImgOverlay, CardText, CardBody, CardTitle } from 'reactstrap';
+import { Card, CardImg, CardImgOverlay, CardText, CardBody, CardTitle,  Breadcrumb, BreadcrumbItem } from 'reactstrap';
+import { Link } from 'react-router-dom';
 
-function RenderDish(dish) {
+function RenderDish({dish}) {
         return (
           <Card>
             <CardImg top src={dish.image} alt={dish.name} />
@@ -39,12 +40,22 @@ function RenderDish(dish) {
         if (props.dish != null) {
           return (
             <div class="container">
+               <div className="row">
+                    <Breadcrumb>
+                        <BreadcrumbItem><Link to="/menu">Menu</Link></BreadcrumbItem>
+                        <BreadcrumbItem active>{props.dish.name}</BreadcrumbItem>
+                    </Breadcrumb>
+                    <div className="col-12">
+                        <h3>{props.dish.name}</h3>
+                        <hr />
+                    </div>                
+                </div>
             <div className="row">
               <div className="col-12 col-md-5 m-1">
                 <RenderDish dish={props.dish} />
               </div>
               <div className="col-12 col-md-5 m-1">
-                <RenderComments comments={props.dish.comments} />
+                <RenderComments comments={props.comments} />
               </div>
             </div>
             </div>
@@ -55,4 +66,4 @@ function RenderDish(dish) {
       }
     
     
-    export default RenderDish;
+    export default DishDetail;
