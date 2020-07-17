b9e8ba3a65acf52de81bf61897acf0553964e171
ShreelekhaRN <shreelekharn8@gmail.com>
2020-07-17 16:03:42 +0530

    combining reducers

--- src/redux/configureStore.js
+++ src/redux/configureStore.js
@@ -1,5 +1,9 @@
 import {createStore} from 'redux';
-import { Reducer, initialState } from './reducer'
+import { Dishes } from './dishes';
+import { Comments } from './comments';
+import { Promotions } from './promotions';
+import { Leaders } from './leaders';
+
 
 export const ConfigureStore = () => {
     const store = createStore(
